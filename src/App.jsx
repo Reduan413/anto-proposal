e6d98c939614945772a9fc4702402506c93496e9
naffiziqbal <f.nafiziqbal@gmail.com>
2025-04-09 12:05:13 +0600

    Service

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import OurService from "./pages/Ourservice";
+import OurService from "./pages/Service/OurService";
 import Proposal from "./proposal";
 
 function App() {
--- src/pages/Service/OurService.jsx
+++ src/pages/Service/OurService.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import "./service.css";
 
 const serviceData = [
   {
